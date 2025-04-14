32d01bb2b5a76355d5a95bf884867b0f8173ad97
Raunak Kumar Singh <skraunak24@tbc.edu.np>
2025-04-14 16:40:00 +0545

    Add files via upload

---
+++ WEEK1_2/Q8.py
@@ -0,0 +1,10 @@
+# Count letters and digits in a string
+text = input("Enter a string: ")
+letters = 0
+digits = 0
+for char in text:
+    if char.isalpha():
+        letters += 1
+    elif char.isdigit():
+        digits += 1
+print("Letters:", letters, "Digits:", digits)
